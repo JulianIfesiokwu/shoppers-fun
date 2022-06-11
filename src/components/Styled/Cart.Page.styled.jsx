81dc733d448946984fe694102ddc9533f395916e
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-06-11 10:55:38 +0100

    created cart page and styling for page

---
+++ src/components/Styled/Cart.Page.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledCartPage = styled.section`
+    padding: 4rem;
+    width: 70%;
+    margin: 0 auto;
+`;
+
+export default StyledCartPage
---
+++ src/pages/Cart/CartPage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledCartPage from "../../components/Styled/Cart.Page.styled";
+
+const CartPage = ({ cart, setCart }) => {
+    return (
+        <StyledCartPage>
+            This is the cart page
+        </StyledCartPage>
+    )
+}
+
+export default CartPage
